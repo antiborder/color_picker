38f4f932f70f6a7f04f46f2805db6ee24acb41f1
Morishita <bafjkfgbka@gmail.com>
2023-07-23 16:47:15 +0700

    Project作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
+import convert from 'color-convert';
+import { Html } from '@react-three/drei';
 
 function App() {
+
+  const cameraPosition = [-2, 0, 7]; // カメラの位置
+  // const squares = [];
+  const square2ds = [];
+
+  const zRange = 8;
+  const yRange = 8;
+  const xRange = 8;
+
+
+  // for (let x = 0; x <= xRange; x++) {
+  //   for (let y = 0; y <= xRange; y++) {
+  //     for (let z = 0; z <= zRange; z++) {
+  //       squares.push(
+  //         <Square
+  //           key={[x, y, z]}
+  //           position={[x, y, z]}
+  //           color={'#' + convert.rgb.hex(x * 255 / xRange, y * 255 / yRange, z * 255 / zRange)}
+  //           emissive={new THREE.Color('#000000')}
+  //         />
+
+  //       )
+  //         ;
+  //     }
+  //   }
+
+  // }
+
+
+  for (let x = 0; x <= xRange; x++) {
+    for (let y = 0; y <= xRange; y++) {
+      for (let z = 0; z <= zRange; z++) {
+        square2ds.push(
+          <Square2d
+            key={[x, y, z]}
+            position={[x, y, z]}
+            color={'#' + convert.rgb.hex(x * 255 / xRange, y * 255 / yRange, z * 255 / zRange)}
+            emissive={new THREE.Color('#000000')}
+          />
+        )
+          ;
+      }
+    }
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex' ,backgroundColor: 'none'}}>
+        <div style={{backgroundColor:'#FF0000',width:'100px', margin:'24px 24px 0px 24px'}}>
+          R
+        </div>
+        <div style={{backgroundColor:'#00FF00',width:'100px', margin:'24px 24px 0px 24px'}}>
+          G
+        </div>
+        <div style={{backgroundColor:'#0000FF',width:'100px', margin:'24px 24px 0px 24px'}}>
+          B
+        </div>
+        <div style={{backgroundColor:'#FFFFFF',width:'100px', margin:'24px 24px 0px 24px'}}>
+          W
+        </div>
+
+      </div>
+      <Canvas
+        camera={{ position: [-2, 0, 20] }}
+        style={{ height: '1000px' }}
+      >
+
+        
+        {/* <meshStandardMaterial color={'orange'}></meshStandardMaterial> */}
+        <color attach="background" args={['#DDDDDD']} />
+        <ambientLight color='#ffffff' intensity={1} />
+        {/* <pointLight position={[1, 1, 1]} /> */}
+
+        <OrbitControls args={[cameraPosition]} />
+        <mesh position={[-2, 0, 0]}>
+
+        <group position={[10,10,10]}>
+          {/* {squares} */}
+          </group>
+            {square2ds}
+          
+        </mesh>
+      </Canvas>
     </div>
   );
 }
 
 export default App;
+
+const Square = ({ size = 0.4, radius = 0, color = '#000000', opacity = 1, ...props }) => {
+  const shape = new THREE.Shape();
+  const x = size / 2 - radius;
+  const y = size / 2 - radius;
+
+  // ポリゴンの形状を作成
+  shape.moveTo(-x, -y + radius);
+  shape.quadraticCurveTo(-x, -y, -x + radius, -y);
+  shape.lineTo(x - radius, -y);
+  shape.quadraticCurveTo(x, -y, x, -y + radius);
+  shape.lineTo(x, y - radius);
+  shape.quadraticCurveTo(x, y, x - radius, y);
+  shape.lineTo(-x + radius, y);
+  shape.quadraticCurveTo(-x, y, -x, y - radius);
+  shape.lineTo(-x, -y + radius);
+
+  // ポリゴンをShapeGeometryで描画
+  const geometry = new THREE.ShapeGeometry(shape);
+
+  // オブジェクトを描画
+  const material = new THREE.MeshStandardMaterial({ color: new THREE.Color(color), transparent: true, opacity, side: THREE.DoubleSide });
+  return (
+    <mesh geometry={geometry} material={material} {...props} />
+  );
+};
+
+
+const Square2d = ({ size = 0.4, radius = 0, color = '#000000', opacity = 1, ...props }) => {
+
+  return (
+    
+    <mesh   {...props} >
+        <Html
+            position={[0, 0, 0]} // position relative to the node
+            zIndexRange={[100, 5]}
+            >
+              <div style={{width:'16px', height:'16px',backgroundColor:color}}></div>
+          </Html>
+          </mesh>
+  );
+};
---
+++ src/colorFunctions.js
@@ -0,0 +1,11 @@
+import { cognitiveFunctionLabels } from "./cognitiveFunctionLabels";
+import { typeLabels } from "./typeLabels";
+
+export function hslToRgb(h, s, v) {
+  let f = (n, k=(n+h/60)%6) => v - v*s*Math.max( Math.min(k,4-k,1), 0);     
+  let rgb = [f(5), f(3), f(1)];
+  return '#' + rgb.map(x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+  }).join('');
+}
